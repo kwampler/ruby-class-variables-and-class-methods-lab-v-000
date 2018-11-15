98401861cb58e0d6a21a507482bca25916bec134
katejsenger <katejsenger@yahoo.com>
2018-11-15 20:38:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,26 @@ class Song
     @@count
   end 
   
-    def self.genres
+  # Control for duplicates when you code your 
+  # .genres class method, not when you add 
+  # genres to the original @@genres array.
+  @@genres = []
+  
+# this should be deleted at some point
+# def self.total_genre_entries
+#   @@genres
+# end
+  
+  def self.genres
     @@genres.uniq
   end
   
+  # this method is the one I found on stackexchange...
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
+  
+  # This is how the method is done on learn.co:
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
@@ -39,4 +55,20 @@ class Song
     @@genres << genre
     @@count += 1
   end
+  attr_accessor :name, :artist, :genre
 end 
+
+the_day = Song.new("The Day", "John Smith", "classic")
+tomorow = Song.new("Tomorow", "John Smith", "classic")
+a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
+a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
+cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
+
+puts the_day.name
+puts the_day.artist
+puts the_day.genre
+puts Song.count
+Song.genres
+Song.artists
+Song.genre_count
+Song.artist_count
